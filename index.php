bec01f31a572b293b7b19cc3536e34f4639a3a07
Patrik Persson <patrik.persson10@tele2.se>
2014-08-03 18:09:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+//Get header
+include ("header.php");
+
+?>
+			<div class="article">
+				<h2>My Moviecollection</h2>
+				<p></p>
+				<p>During the years I have seen a large amount of different movies.
+				On this site I have collect all the movies that I have seen during the years.</p> 
+			
+				<p>I am a big fan of Sci-fi movies and action movies. 
+				I grow up with Star Wars and James Bond.</p> 
+			
+				<p>I also like old movies like Space: 1999, old Star-Trek series and The Dynamic Duo with Batman and Robin.</p> 
+			
+				<br> 
+				<p>The images from the slideshow is from <a href="http://www.themoviedb.org/">themoviedb.org</a></p>
+			</div>
+			
+			<div class="slideshow">
+				
+				<!--Slideshow-->
+				<div id="slideshow">
+					<img src="image1.jpg" alt="Slideshow Image 1" class="active" />
+					<img src="image2.jpg" alt="Slideshow Image 2" />
+					<img src="image3.jpg" alt="Slideshow Image 3" />
+					<img src="image4.jpg" alt="Slideshow Image 4" />
+					<img src="image5.jpg" alt="Slideshow Image 5" />
+					<img src="image6.jpg" alt="Slideshow Image 6" />
+					<img src="image7.jpg" alt="Slideshow Image 7" />
+				</div>
+		
+		</div>
+			
+			
+<?php
+//Get footer
+include ("footer.php");
+
+?>	
