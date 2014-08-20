af8280fd6baff7ff7d47c723a17721cf01b48806
Patrik Persson <patrik.persson10@tele2.se>
2014-08-20 11:46:35 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php
+//Get header
+include ("header.php");
+
+?>
+	
+	<h2>Contact</h2>
+	<p>You can contact my by using <a href = "#">patrik.persson10@tele2.se</a></p>
+
+<?php
+//Get footer
+include ("footer.php");
+
+?>	
