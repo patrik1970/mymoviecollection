4242152146bef101a0b0be1bf0943a5d763fc011
Patrik Persson <patrik.persson10@tele2.se>
2014-08-20 11:36:32 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+//Get header
+include ("header.php");
+
+?>
+	<h2>About My Moviecollection</h2>
+	<p></p>
+	<p>My Moviecolletion is a site for collection information about all the movies that I have seen during all this years.
+	I have a idea to share information about movies and especially movies that I have seen. 
+	I also rate all the movies to show what type of movies that I like best.  
+			
+	<p>It's also a site to show what I have learn during my 3 years at Malmö University as a programmer.
+	I made this site during a webbprogrammer course at Malmö University in the autumn 2013.</p> 
+
+<?php
+//Get footer
+include ("footer.php");
+
+?>	
