324df0b13bdb8a85d80e370d71b25f4c64231fff
Patrik Persson <patrik.persson10@tele2.se>
2014-08-31 16:51:23 +0200

    Update movietips.php

--- movietips.php
+++ movietips.php
@@ -21,44 +21,44 @@ include ("header.php");
 			</tr>
 
 <?php
-	//Database management.
-	//THe connection against the database through mysql_connect("Server(host)","Username","Password","Database").
-	$databas1 = mysql_connect("Server(host)","Username","Password","Database");
-	mysql_select_db("Database");//Select the valid database.
+			//Database management.
+			//THe connection against the database through mysql_connect("Server(host)","Username","Password","Database").
+			$databas1 = mysql_connect("Server(host)","Username","Password","Database");
+			mysql_select_db("Database");//Select the valid database.
 	
-	//The if-statement check the possibility to connect to the server.
-	if (!$databas1) 
-	{
-		//echo "<p>Could not connect to the server </p>";
-        echo mysql_error();
-    }
-	else
-	{
-		//echo "<p>Successfully connected to the server </p>";
-	}
+			//The if-statement check the possibility to connect to the server.
+			if (!$databas1) 
+			{
+				//echo "<p>Could not connect to the server </p>";
+        			echo mysql_error();
+    			}
+			else
+			{
+				//echo "<p>Successfully connected to the server </p>";
+			}
 	  
-	//Database question.
-	$fraga = "SELECT title, grade, year FROM movies LIMIT 10";
-	$resultat = mysql_query($fraga); 
+			//Database question.
+			$fraga = "SELECT title, grade, year FROM movies LIMIT 10";
+			$resultat = mysql_query($fraga); 
     
-	//The if-statement check if the question is executable.
-	if (false === $resultat) 
-	{ 
-		echo mysql_error(); 
-	}
+			//The if-statement check if the question is executable.
+			if (false === $resultat) 
+			{ 
+				echo mysql_error(); 
+			}
 	  
-	//Show the answer of the question.
-	while ($kolumn = mysql_fetch_array($resultat))
-    { 
-		echo 	" <tr>" . 
+			//Show the answer of the question.
+			while ($kolumn = mysql_fetch_array($resultat))
+    			{ 
+				echo 	" <tr>" . 
 				" <td>$kolumn[0]</td>" . 
 				" <td>$kolumn[1]</td>" . 
 				" <td>$kolumn[2]</td>" .
 				" </tr>" ; 
-	}
+			}
 	
-	//Close the database
-	mysql_close($databas1);
+			//Close the database
+			mysql_close($databas1);
 ?>
 		</table>
 	</div>
